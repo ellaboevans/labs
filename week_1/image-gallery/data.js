a9e0dd36ab1decf8647e7f4b58c274d72bc8ff63
Evans Elabo <evans.elabo@amalitech.com>
2024-11-06 08:40:41 +0000

    refactor(app): update image datasets

--- week_1/image-gallery/data.js
+++ week_1/image-gallery/data.js
@@ -1,100 +1,93 @@
 export const IMAGES = [
   {
     thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2018/01/09/20/24/wood-3072434_1280.jpg",
-    caption: "Wood, Forest, Light image",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2018/01/09/20/24/wood-3072434_1280.jpg",
-  },
-  {
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2020/03/02/20/59/tree-4896935_1280.jpg",
-    caption: "Wood, Forest, Light image",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2020/03/02/20/59/tree-4896935_1280.jpg",
-  },
-  {
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2022/06/07/16/22/drift-7248723_1280.jpg",
-    caption: "Wood, Forest, Light image",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2022/06/07/16/22/drift-7248723_1280.jpg",
-  },
-  {
-    thumbnailUrl:
       "https://cdn.pixabay.com/photo/2023/03/10/08/09/woman-7841725_1280.jpg",
-    caption: "Wood, Forest, Light image",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2023/03/10/08/09/woman-7841725_1280.jpg",
-  },
-  {
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2020/03/02/20/59/tree-4896935_1280.jpg",
-    caption: "Wood, Forest, Light image",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2020/03/02/20/59/tree-4896935_1280.jpg",
-  },
-  {
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2022/06/07/16/22/drift-7248723_1280.jpg",
-    caption: "Wood, Forest, Light image",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2022/06/07/16/22/drift-7248723_1280.jpg",
-  },
-  {
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2023/03/10/08/09/woman-7841725_1280.jpg",
-    caption: "Wood, Forest, Light image",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2023/03/10/08/09/woman-7841725_1280.jpg",
-  },
-  {
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2020/03/02/20/59/tree-4896935_1280.jpg",
-    caption: "Wood, Forest, Light image",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2020/03/02/20/59/tree-4896935_1280.jpg",
-  },
-  {
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2022/06/07/16/22/drift-7248723_1280.jpg",
-    caption: "Wood, Forest, Light image",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2022/06/07/16/22/drift-7248723_1280.jpg",
-  },
-  {
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2023/03/10/08/09/woman-7841725_1280.jpg",
-    caption: "Wood, Forest, Light image",
+    caption: "Woman, nature, and freedom",
     imageUrl:
       "https://cdn.pixabay.com/photo/2023/03/10/08/09/woman-7841725_1280.jpg",
   },
   {
     thumbnailUrl:
       "https://cdn.pixabay.com/photo/2022/06/07/16/22/drift-7248723_1280.jpg",
-    caption: "Wood, Forest, Light image",
+    caption: "Speed, drfit, transport",
     imageUrl:
       "https://cdn.pixabay.com/photo/2022/06/07/16/22/drift-7248723_1280.jpg",
   },
   {
     thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2023/03/10/08/09/woman-7841725_1280.jpg",
-    caption: "Wood, Forest, Light image",
+      "https://cdn.pixabay.com/photo/2024/10/22/01/17/cat-9138461_1280.jpg",
+    caption: "Cat, American shorthair",
     imageUrl:
-      "https://cdn.pixabay.com/photo/2023/03/10/08/09/woman-7841725_1280.jpg",
+      "https://cdn.pixabay.com/photo/2024/10/22/01/17/cat-9138461_1280.jpg",
   },
   {
     thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2022/06/07/16/22/drift-7248723_1280.jpg",
-    caption: "Wood, Light image",
+      "https://cdn.pixabay.com/photo/2023/08/08/09/21/couple-8176869_1280.jpg",
+    caption: "Couple, marriage, wedding",
     imageUrl:
-      "https://cdn.pixabay.com/photo/2022/06/07/16/22/drift-7248723_1280.jpg",
+      "https://cdn.pixabay.com/photo/2023/08/08/09/21/couple-8176869_1280.jpg",
   },
   {
     thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2023/03/10/08/09/woman-7841725_1280.jpg",
-    caption: "Wood, Forest, Light image",
+      "https://cdn.pixabay.com/photo/2024/10/11/19/50/animal-9113761_1280.jpg",
+    caption: "Animal, mammal, horses",
     imageUrl:
-      "https://cdn.pixabay.com/photo/2023/03/10/08/09/woman-7841725_1280.jpg",
+      "https://cdn.pixabay.com/photo/2024/10/11/19/50/animal-9113761_1280.jpg",
+  },
+  {
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2023/06/13/16/25/baby-8061294_1280.jpg",
+    caption: "Baby infant",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2023/06/13/16/25/baby-8061294_1280.jpg",
+  },
+  {
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2023/05/25/10/35/bridge-8016809_1280.jpg",
+    caption: "Bridge, nature, sea",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2023/05/25/10/35/bridge-8016809_1280.jpg",
+  },
+  {
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2024/09/22/09/39/pantheon-paris-9065570_1280.jpg",
+    caption: "building, paris",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2024/09/22/09/39/pantheon-paris-9065570_1280.jpg",
+  },
+  {
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2022/12/02/16/52/the-path-7631282_1280.jpg",
+    caption: "the path, forest, trees",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2022/12/02/16/52/the-path-7631282_1280.jpg",
+  },
+  {
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2024/01/24/13/14/boat-8529554_1280.jpg",
+    caption: "lake, both, orion blues",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2024/01/24/13/14/boat-8529554_1280.jpg",
+  },
+  {
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2024/08/28/09/34/bridge-9003553_1280.jpg",
+    caption: "river, bridge, structure",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2024/08/28/09/34/bridge-9003553_1280.jpg",
+  },
+  {
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2023/12/11/12/03/exit-sign-8443453_1280.jpg",
+    caption: "Exit sign, Exit light image",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2023/12/11/12/03/exit-sign-8443453_1280.jpg",
+  },
+  {
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2022/12/05/22/08/open-book-7637805_1280.jpg",
+    caption: "open book, nostalgia",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2022/12/05/22/08/open-book-7637805_1280.jpg",
   },
 ];
