36e4cc0097e5ace71d155b8600848b4fe0cb26f5
Evans Elabo <evans.elabo@amalitech.com>
2024-11-20 09:25:33 +0000

    refactor(app): organize codebase to ensure readability

--- week_3/dictionary-app/scripts/theme.js
+++ week_3/dictionary-app/scripts/theme.js
@@ -1,12 +1,17 @@
+// Select HTML element
 const toggleMenu = document.getElementById("toggle__menu");
 
+// Handle theme change
 function handleThemeChange() {
-  localStorage.setItem("theme", toggleMenu.checked ? "dark" : "light");
-  document.body.classList.toggle("dark-theme", toggleMenu.checked);
+  const isDarkTheme = toggleMenu.checked;
+  localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
+  document.body.classList.toggle("dark-theme", isDarkTheme);
 }
 
+// Event listener for theme toggle
 toggleMenu.addEventListener("change", handleThemeChange);
 
+// Set initial theme based on local storage
 if (localStorage.getItem("theme") === "dark") {
   toggleMenu.checked = true;
   document.body.classList.add("dark-theme");
