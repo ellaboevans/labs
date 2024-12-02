13d221f6d6a182b545ec6860262b043a2b3f2c06
Evans Elabo <evans.elabo@amalitech.com>
2024-12-02 19:47:30 +0000

    refactor(app): remove routes and global detection providers

--- week_4/quiz-app/src/app/app.config.ts
+++ week_4/quiz-app/src/app/app.config.ts
@@ -1,13 +1,7 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
 
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(),
-  ],
+  providers: [provideHttpClient()],
 };
