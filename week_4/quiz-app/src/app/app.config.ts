dba3ec962da9340dfe2679984cde11d637ad81ab
Evans Elabo <evans.elabo@amalitech.com>
2024-12-04 17:01:40 +0000

    refactor(app): remove provider client imports

--- week_4/quiz-app/src/app/app.config.ts
+++ week_4/quiz-app/src/app/app.config.ts
@@ -1,7 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
 
-import { provideHttpClient } from '@angular/common/http';
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient()],
+  providers: [],
 };
