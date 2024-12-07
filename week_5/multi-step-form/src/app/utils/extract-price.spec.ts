33f99895db2db330c6de592923a205c5beca573c
Evans <ellaboevans@gmail.com>
2024-12-07 22:30:48 +0000

    refactor(app): Enhanced tests for edge cases including null input

--- week_5/multi-step-form/src/app/utils/extract-price.spec.ts
+++ week_5/multi-step-form/src/app/utils/extract-price.spec.ts
@@ -1,26 +1,41 @@
 import { extractPrice } from './extract-price';
 
 describe('extractPrice', () => {
+  let result: number;
+
   it('should return 0 if the input is undefined', () => {
-    expect(extractPrice(undefined!)).toBe(0);
+    result = extractPrice(undefined!);
+    expect(result).toBe(0);
+  });
+
+  it('should return 0 if the input is null', () => {
+    result = extractPrice(null);
+    expect(result).toBe(0);
   });
 
   it('should return the numeric part of a valid price string with no decimals', () => {
-    expect(extractPrice('100$')).toBe(100);
-    expect(extractPrice('Price is 100 dollars')).toBe(100);
+    result = extractPrice('100$');
+    expect(result).toBe(100);
+    result = extractPrice('Price is 100 dollars');
+    expect(result).toBe(100);
   });
 
-  it('should return the numeric part of a vaid price string with decimals', () => {
-    expect(extractPrice('100.50$')).toBe(100.5);
-    expect(extractPrice('$99.99')).toBe(99.99);
-    expect(extractPrice('Price is 100.50 dollars')).toBe(100.5);
+  it('should return the numeric part of a valid price string with decimals', () => {
+    result = extractPrice('100.50$');
+    expect(result).toBe(100.5);
+    result = extractPrice('$99.99');
+    expect(result).toBe(99.99);
+    result = extractPrice('Price is 100.50 dollars');
+    expect(result).toBe(100.5);
   });
 
   it('should return 0 for strings without any numeric values', () => {
-    expect(extractPrice('hello world')).toBe(0);
+    result = extractPrice('hello world');
+    expect(result).toBe(0);
   });
 
   it('should return 0 for empty strings', () => {
-    expect(extractPrice(' ')).toBe(0);
+    result = extractPrice(' ');
+    expect(result).toBe(0);
   });
 });
--- week_5/multi-step-form/src/app/utils/extract-price.ts
+++ week_5/multi-step-form/src/app/utils/extract-price.ts
@@ -1,7 +1,12 @@
 //Function to extract the numeric part from the price string
-export function extractPrice(priceString: string): number {
-  if (typeof priceString === 'undefined') return 0;
+export function extractPrice(priceString: string | undefined | null): number {
+  if (
+    typeof priceString === 'undefined' ||
+    priceString === undefined ||
+    priceString?.trim() === ''
+  )
+    return 0;
 
-  const match = priceString.match(/\d+(\.\d+)?/);
+  const match = priceString?.match(/\d+(\.\d+)?/);
   return match ? parseFloat(match[0]) : 0;
 }
