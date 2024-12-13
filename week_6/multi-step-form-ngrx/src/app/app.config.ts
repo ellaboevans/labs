07e783bea0c55977df37b3bbc559f800635dd6a8
Evans Elabo <evans.elabo@amalitech.com>
2024-12-13 15:49:31 +0000

    refactor(app): re-configure app entry to use ngrx providers

---
+++ week_6/multi-step-form-ngrx/src/app/app.config.ts
@@ -0,0 +1,28 @@
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { formFeatureKey, formReducer } from './store/form/form.reducers';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore(),
+    provideState(formFeatureKey, formReducer),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+      connectInZone: true,
+    }),
+  ],
+};
