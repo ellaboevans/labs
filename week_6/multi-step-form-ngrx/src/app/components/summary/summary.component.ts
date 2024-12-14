1e978b0d953d6a4b9c17bb4d8558acfb26d8f981
Evans Elabo <evans.elabo@amalitech.com>
2024-12-14 15:09:14 +0000

    refactor(app): update component to use state selectors

--- week_6/multi-step-form-ngrx/src/app/components/summary/summary.component.ts
+++ week_6/multi-step-form-ngrx/src/app/components/summary/summary.component.ts
@@ -1,9 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { CapitalizePipe } from '../../pipes/capitalize.pipe';
 import { Summary } from '../../interface/summary';
 import { Store } from '@ngrx/store';
-import { selectIsYearly } from '../../store/form/form.reducers';
+import {
+  selectCurrentStep,
+  selectFormFields,
+  selectIsYearly,
+} from '../../state/form/form.selector';
+import { extractPrice } from '../../utils/extract-price';
+import { Addon, Plan } from '../../interface/addon';
 
 @Component({
   selector: 'app-summary',
@@ -15,13 +21,35 @@ import { selectIsYearly } from '../../store/form/form.reducers';
 export class SummaryComponent {
   private readonly store = inject(Store);
 
-  @Input({ required: true }) public summary!: Summary;
-  @Input({ required: true }) public totalPrice: string = '';
   @Output() public stepChange = new EventEmitter<number>();
+
+  protected totalPrice!: string;
 
   public readonly isYearly = this.store.selectSignal(selectIsYearly);
 
+  public readonly currentStep = this.store.selectSignal(selectCurrentStep);
+
+  public readonly formFields = this.store.selectSignal(selectFormFields);
+
   public goToStepTwo(): void {
     this.stepChange.emit(2);
+  }
+
+  public get calculateTotalPrice(): string {
+    const planPrice = extractPrice(this.summaryData?.plan?.price);
+    const addonPrices = this.summaryData?.addons?.reduce(
+      (total: number, addon: Addon) => {
+        return total + extractPrice(addon.price);
+      },
+      0
+    );
+    const total = planPrice + addonPrices!;
+    return (this.totalPrice = this.isYearly()
+      ? `$${total}/yr`
+      : `$${total}/mo`);
+  }
+
+  public get summaryData(): Summary | undefined {
+    return this.formFields();
   }
 }
