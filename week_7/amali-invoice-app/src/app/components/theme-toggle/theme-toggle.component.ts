53b4b3f7ed4fa7df4ba9e5a2e0df0ef1229758ac
Evans Elabo <evans.elabo@amalitech.com>
2024-12-18 22:04:41 +0000

    fix(app): adjust and implement them toggle logic

--- week_7/amali-invoice-app/src/app/components/theme-toggle/theme-toggle.component.ts
+++ week_7/amali-invoice-app/src/app/components/theme-toggle/theme-toggle.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ThemeServiceService } from '../../services/theme-service/theme.service';
 
 @Component({
   selector: 'app-theme-toggle',
@@ -7,10 +8,20 @@ import { Component, signal } from '@angular/core';
   templateUrl: './theme-toggle.component.html',
   styleUrl: './theme-toggle.component.css',
 })
-export class ThemeToggleComponent {
+export class ThemeToggleComponent implements OnInit {
   public isToggle = signal<Boolean>(false);
+
+  private readonly themeService = inject(ThemeServiceService);
+
+  ngOnInit(): void {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      document.documentElement.setAttribute('data-theme', savedTheme);
+    }
+  }
 
   public toggleTheme(): void {
     this.isToggle.update((prev) => !prev);
+    this.themeService.toggleTheme();
   }
 }
