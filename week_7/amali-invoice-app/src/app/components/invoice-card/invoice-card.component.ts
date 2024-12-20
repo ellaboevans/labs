dd7c257a6baedb1c7e2de12782dd5b0461cc4649
Evans Elabo <evans.elabo@amalitech.com>
2024-12-20 09:07:39 +0000

    feat(app): add router to card item

--- week_7/amali-invoice-app/src/app/components/invoice-card/invoice-card.component.ts
+++ week_7/amali-invoice-app/src/app/components/invoice-card/invoice-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { BadgeComponent } from '../../shared/components/badge/badge.component';
 import { Invoice } from '../../shared/interface/invoice.interface';
 import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-invoice-card',
@@ -12,8 +13,9 @@ import { CommonModule } from '@angular/common';
 })
 export class InvoiceCardComponent {
   @Input() public invoice!: Invoice;
+  private readonly router = inject(Router);
 
   public navigateToInvoice(id: string): void {
-    console.log('Invoice ID:', id);
+    this.router.navigateByUrl(`/invoices/${id}`);
   }
 }
