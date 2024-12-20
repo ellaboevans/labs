3493c2534d8b6e5627b3d8e2e4da3fa5c9a63db6
Evans Elabo <evans.elabo@amalitech.com>
2024-12-20 09:12:25 +0000

    refactor(app): remove static data

--- week_7/amali-invoice-app/src/app/layout/layout.component.ts
+++ week_7/amali-invoice-app/src/app/layout/layout.component.ts
@@ -1,122 +1,14 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../components/sidebar/sidebar.component';
 import { ContentComponent } from '../components/content/content.component';
-import { HeaderComponent } from '../components/header/header.component';
-import { InvoiceCardComponent } from '../components/invoice-card/invoice-card.component';
-import { Invoice } from '../shared/interface/invoice.interface';
+
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [
-    SidebarComponent,
-    ContentComponent,
-    HeaderComponent,
-    InvoiceCardComponent,
-  ],
+  imports: [SidebarComponent, ContentComponent, RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
-export class LayoutComponent {
-  public readonly invoices: Invoice[] = [
-    {
-      id: 'RT3080',
-      createdAt: '2021-08-18',
-      paymentDue: '2021-08-19',
-      description: 'Re-branding',
-      paymentTerms: 1,
-      clientName: 'Jensen Huang',
-      clientEmail: 'jensenh@mail.com',
-      status: 'paid',
-      senderAddress: {
-        street: '19 Union Terrace',
-        city: 'London',
-        postCode: 'E1 3EZ',
-        country: 'United Kingdom',
-      },
-      clientAddress: {
-        street: '106 Kendell Street',
-        city: 'Sharrington',
-        postCode: 'NR24 5WQ',
-        country: 'United Kingdom',
-      },
-      items: [
-        {
-          name: 'Brand Guidelines',
-          quantity: 1,
-          price: 1800.9,
-          total: 1800.9,
-        },
-      ],
-      total: 1800.9,
-    },
-    {
-      id: 'XM9141',
-      createdAt: '2021-08-21',
-      paymentDue: '2021-09-20',
-      description: 'Graphic Design',
-      paymentTerms: 30,
-      clientName: 'Alex Grim',
-      clientEmail: 'alexgrim@mail.com',
-      status: 'pending',
-      senderAddress: {
-        street: '19 Union Terrace',
-        city: 'London',
-        postCode: 'E1 3EZ',
-        country: 'United Kingdom',
-      },
-      clientAddress: {
-        street: '84 Church Way',
-        city: 'Bradford',
-        postCode: 'BD1 9PB',
-        country: 'United Kingdom',
-      },
-      items: [
-        {
-          name: 'Banner Design',
-          quantity: 1,
-          price: 156.0,
-          total: 156.0,
-        },
-        {
-          name: 'Email Design',
-          quantity: 2,
-          price: 200.0,
-          total: 400.0,
-        },
-      ],
-      total: 556.0,
-    },
-    {
-      id: 'RG0314',
-      createdAt: '2021-09-24',
-      paymentDue: '2021-10-01',
-      description: 'Website Redesign',
-      paymentTerms: 7,
-      clientName: 'John Morrison',
-      clientEmail: 'jm@myco.com',
-      status: 'draft',
-      senderAddress: {
-        street: '19 Union Terrace',
-        city: 'London',
-        postCode: 'E1 3EZ',
-        country: 'United Kingdom',
-      },
-      clientAddress: {
-        street: '79 Dover Road',
-        city: 'Westhall',
-        postCode: 'IP19 3PF',
-        country: 'United Kingdom',
-      },
-      items: [
-        {
-          name: 'Website Redesign',
-          quantity: 1,
-          price: 14002.33,
-          total: 14002.33,
-        },
-      ],
-      total: 14002.33,
-    },
-  ];
-}
+export class LayoutComponent {}
