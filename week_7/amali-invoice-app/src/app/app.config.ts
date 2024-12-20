7c3e328e4dc48b31fcea400c33211991b563bd05
Evans Elabo <evans.elabo@amalitech.com>
2024-12-20 09:00:55 +0000

    feat(app): add neccessary providers to keep the store working

--- week_7/amali-invoice-app/src/app/app.config.ts
+++ week_7/amali-invoice-app/src/app/app.config.ts
@@ -1,8 +1,36 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import {
+  invoiceReducer,
+  invoiceReducerName,
+} from './states/invoice/invoice.reducers';
+import { provideEffects } from '@ngrx/effects';
+import { InvoiceEffects } from './states/invoice/invoice.effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideStore(),
+    provideState(invoiceReducerName, invoiceReducer),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 25,
+      connectInZone: true,
+    }),
+    provideEffects(InvoiceEffects),
+  ],
 };
