462f237287487556f7b1f25303ff537b06f6efab
Evans Elabo <evans.elabo@amalitech.com>
2024-12-20 09:01:23 +0000

    feat(app): add routes for navigation

--- week_7/amali-invoice-app/src/app/app.routes.ts
+++ week_7/amali-invoice-app/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
+import { InvoiceDetailsComponent } from './components/invoice-details/invoice-details.component';
+import { InvoiceListsComponent } from './components/invoice-lists/invoice-lists.component';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'dashboard',
+    redirectTo: 'invoices',
+  },
+  {
+    path: 'invoices',
+    component: InvoiceListsComponent,
+  },
+  {
+    path: 'invoices/:id',
+    component: InvoiceDetailsComponent,
   },
 ];
