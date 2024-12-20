f0cc526688ec07b68432c2124f5857a97aa3ecd5
Evans Elabo <evans.elabo@amalitech.com>
2024-12-20 16:42:50 +0000

    refactor(app): dynamically render statuses

---
+++ week_7/amali-invoice-app/src/app/components/filter/filter.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject, signal } from '@angular/core';
+import { HeadlineComponent } from '../../shared/components/headline/headline.component';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { invoiceActions } from '../../states/invoice/invoice.actions';
+
+@Component({
+  selector: 'app-filter',
+  standalone: true,
+  imports: [HeadlineComponent, CommonModule],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.css',
+})
+export class FilterComponent {
+  public isOpen = signal(false);
+  private readonly store = inject(Store);
+
+  public toggleDropdown(): void {
+    this.isOpen.set(!this.isOpen());
+  }
+
+  public onStatusChange(status: string, event: Event): void {
+    const isChecked = (event.target as HTMLInputElement).checked;
+    if (isChecked) {
+      this.store.dispatch(invoiceActions.setInvoiceStatusFilter({ status }));
+    } else {
+      this.store.dispatch(invoiceActions.clearInvoiceFilter());
+    }
+  }
+}
