f3113744763ab9898d5d25df329e1daf508914f7
Evans Elabo <evans.elabo@amalitech.com>
2024-12-20 09:05:11 +0000

    feat(app): consume selectors from store

--- week_7/amali-invoice-app/src/app/components/header/header.component.ts
+++ week_7/amali-invoice-app/src/app/components/header/header.component.ts
@@ -1,6 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { HeadlineComponent } from '../../shared/components/headline/headline.component';
 import { ButtonComponent } from '../../shared/components/button/button.component';
+import { Store } from '@ngrx/store';
+import {
+  selectError,
+  selectInvoiceCount,
+} from '../../states/invoice/invoice.reducers';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +17,20 @@ import { ButtonComponent } from '../../shared/components/button/button.component
 export class HeaderComponent {
   @Input() public readonly buttonIcon = '/assets/icon-plus.svg';
 
+  private readonly store = inject(Store);
+
+  public readonly invoiceCount = this.store.selectSignal(selectInvoiceCount);
+
+  public readonly error = this.store.selectSignal(selectError);
+
   public newInvoice() {
     console.log('New Invoice');
+    console.log(this.error());
+  }
+
+  get invoiceLabel(): string {
+    return this.invoiceCount() > 0
+      ? `There are ${this.invoiceCount()} invoices`
+      : 'No invoices';
   }
 }
