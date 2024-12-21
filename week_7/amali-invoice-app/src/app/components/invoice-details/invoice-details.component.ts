e12f496f167c1f2a9e9248a6e407588b063b57a1
Evans Elabo <evans.elabo@amalitech.com>
2024-12-21 06:46:39 +0000

    feat(app): build invioice details status card

--- week_7/amali-invoice-app/src/app/components/invoice-details/invoice-details.component.ts
+++ week_7/amali-invoice-app/src/app/components/invoice-details/invoice-details.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { invoiceActions } from '../../states/invoice/invoice.actions';
+import { selectSelectedInvoice } from '../../states/invoice/invoice.reducers';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { Location } from '@angular/common';
+import { BadgeComponent } from '../../shared/components/badge/badge.component';
 
 @Component({
   selector: 'app-invoice-details',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, BadgeComponent],
   templateUrl: './invoice-details.component.html',
-  styleUrl: './invoice-details.component.css'
+  styleUrl: './invoice-details.component.css',
 })
-export class InvoiceDetailsComponent {
+export class InvoiceDetailsComponent implements OnInit {
+  public readonly invoiceId: string = '';
+  private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly location = inject(Location);
 
+  private readonly store = inject(Store);
+  public readonly buttonIcon = '/assets/icon-arrow-left.svg';
+  public readonly invoice = this.store.selectSignal(selectSelectedInvoice);
+
+  constructor() {
+    this.invoiceId = this.activatedRoute.snapshot.params['id'];
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(invoiceActions.getInvoice({ id: this.invoiceId }));
+  }
+
+  public goBack(): void {
+    this.location.back();
+  }
+
+  public updateInvoice(): void {
+    alert(`Update Invoice ${this.invoiceId}`);
+  }
+
+  public deleteInvoice(): void {
+    alert(`Delete Invoice ${this.invoiceId}`);
+  }
+
+  public markInvoiceAsPaid(): void {
+    alert(`Mark Invoice ${this.invoiceId} as Paid`);
+  }
 }
